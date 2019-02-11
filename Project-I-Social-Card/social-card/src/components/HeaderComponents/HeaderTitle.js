6f9986d7d0591df956462d9071045591dd92cade
JaySebagh <jaysebagh@gmail.com>
2019-02-11 13:38:01 -0800

    added header

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,19 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle= () => {
+    return (
+        <div className="header">
+            <div className="textWrapper">
+                <h1 className="name">Lambda School</h1>
+                <h2 className="date">@LambdaSchool - 26 Jan</h2>
+            </div>
+            <div className="text">
+                <p classaName="tweet">Let's learn React by building simple interfaces with components. Don't try to overthink it, just keep it simple and have fun. Once you feel comfortable using components you are well on your way to mastering React!</p>
+            </div>
+        </div>
+    );
+  };
+  
+  export default HeaderTitle;
+  
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Header.css';
+import img from './HeaderImg.png'
+
+const ImageThumbnail= () => {
+    return (
+      <div>
+          <img className="icon" src={img} alt={"LambdaSchool"}></img>
+      </div>
+    );
+  };
+  
+  export default ImageThumbnail;
+  
