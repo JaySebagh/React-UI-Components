45210d91b519daf048c9c9a07d77c9e0a04b72a8
JaySebagh <jaysebagh@gmail.com>
2019-02-11 12:35:28 -0800

    added card banner image

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Card.css';
+import img from './CardImg.png'
+
+
+const CardBanner = () => {
+    return (
+      <div>
+        <img src={img}></img>
+      </div>
+    );
+  };
+  
+  export default CardBanner;
--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Card.css';
+
+
+const CardContainer = () => {
+    return (
+      <div>
+        <h3>test2</h3>
+      </div>
+    );
+  };
+  
+  export default CardContainer;
+  
